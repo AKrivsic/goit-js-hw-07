9812f47d483466355d7d70bd8b5aa13554f049a7
Alex <o.kryvshych@gmail.com>
2025-03-23 01:50:49 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -5,14 +5,18 @@ registerForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const login = form.elements.email.value.trim();
+  const email = form.elements.email.value.trim();
   const password = form.elements.password.value.trim();
   
-    if (login === "" || password === "") {
+    if (email === "" || password === "") {
       alert("All form fields must be filled in");
         return;
   }
 
-  console.log(`Login: ${login}, Password: ${password}`);
+  const loginPass = {
+    email,
+    password
+  };
+  console.log(loginPass);
   form.reset();
 }
